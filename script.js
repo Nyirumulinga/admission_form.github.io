801043e5864d429069b7cac0e31b2f7c2c5cc7a8
Nyirumulinga <kenntyfabrice8@gmail.com>
2023-09-22 00:44:20 +0200

    Added new files

---
+++ script.js
@@ -0,0 +1,9 @@
+function validateForm() {
+    // Add your form validation logic here
+    return true; // Return true if the form is valid
+}
+
+function validateLogin() {
+    // Add your login form validation logic here
+    return true; // Return true if the form is valid
+}
